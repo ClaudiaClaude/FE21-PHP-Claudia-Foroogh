d9e10465a8c11c3763a705ffcbf6b32d84b647cc
Claudia Tomassetti <hagol.cker@gmail.com>
2021-08-09 12:07:22 +0200

    adds basic exercises

---
+++ Claudia/classwork.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- external CSS -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+</head>
+<body>
+
+<!-- Exercise Basic 01 -->
+
+<!-- <div class="myName">
+    <?php 
+            
+        $name = "Claudia";
+        echo "<h1>$name</h1>";
+
+        ?>
+</div> -->
+
+<!-- END Exercise Basic 01 -->
+
+
+<!--Exercise Basic 02  -->
+
+<!-- <div class="myName">
+    <?php 
+            
+        $name = "Claudia";
+        $age = "32";
+        $job = "Web developer";
+
+        echo "Hi, my name is $name, and I am $age years old, and I (want to) work as a $job";
+
+        ?>
+</div> -->
+
+<!-- END Exercise Basic 02 -->
+
+
+<!--Exercise Basic 03  -->
+
+<div class="myName">
+<?php 
+            
+        $players = array("Mark", "John", "Georg", "Lisa");
+
+        echo "The third player in the Team is $players[2]";
+    
+            ?>
+    </div>
+
+<!-- END Exercise Basic 03 -->
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+</body>
+</html>
